715e2edfb8e337f90458433913b7e11b7ed80d04
Kurt Carabott <kurt.carabott@gmail.com>
2019-12-24 12:28:27 +0100

    Initial check-in of module AllShareLive

---
+++ AllShareLive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AllShareLive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
